415d2dadcf679cc89d561e0f4ae53f456dbadafb
joy <ss13867776368@163.com>
2020-04-19 22:21:42 +0800

    update hw2: add cfg in train.py

--- hw2/train.py
+++ hw2/train.py
@@ -1,27 +1,30 @@
 import os
 import numpy as np
 import gfootball.env as football_env
+from torch.utils.tensorboard import SummaryWriter
 
 from dqn import DQN
-from torch.utils.tensorboard import SummaryWriter
+from create_logger import create_logger, print_and_log
 
 
 def trainDQN():
-    device = 'cpu'
-    min_epsilon = 0.01
-    max_epsilon = 0.08
-    max_episodes = 10000
-    print_interval = 20
-    learning_start = 5
-    learning_freq = 1
-    test_freq = 100
-    test_episodes = 10
+    cfg = {}
+    cfg['device'] = 'cpu'
+    cfg['min_epsilon'] = 0.01
+    cfg['max_epsilon'] = 0.08
+    cfg['max_episodes'] = 10000
+    cfg['print_interval'] = 20
+    cfg['learning_start'] = 5
+    cfg['learning_freq'] = 1
+    cfg['test_freq'] = 100
+    cfg['test_episodes'] = 10
 
     cur_dir = os.path.abspath(os.curdir)
-    output_path = os.path.join(cur_dir, 'output')
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-    writer = SummaryWriter(log_dir=os.path.join(output_path, 'tb'))
+    root_output_path = os.path.join(cur_dir, 'output')
+    if not os.path.exists(root_output_path):
+        os.mkdir(root_output_path)
+    final_output_path = create_logger(root_output_path, )
+    writer = SummaryWriter(log_dir=os.path.join(final_output_path, 'tb'))
 
     env = football_env.create_environment(env_name="academy_empty_goal",
                                           representation='simple115',
@@ -42,12 +45,12 @@ def trainDQN():
                 input_type='vector',
                 input_feature=115,
                 random_action=True,
-                device=device)
+                device=cfg.device)
 
     score = 0.0
     training_steps = 0
-    for i_episode in range(max_episodes):
-        epsilon = max(min_epsilon, max_epsilon - 0.01 * (i_episode / 200))
+    for i_episode in range(cfg.max_episodes):
+        epsilon = max(cfg.min_epsilon, cfg.max_epsilon - 0.01 * (i_episode / 200))
         obs = env.reset()
         steps = 0
         epi_reward = 0.0
@@ -64,7 +67,7 @@ def trainDQN():
             epi_reward += reward
             obs = next_obs
 
-        if i_episode >= learning_start and i_episode % learning_freq == 0:
+        if i_episode >= cfg.learning_start and i_episode % cfg.learning_freq == 0:
             epi_loss = model.learn()
             writer.add_scalar('loss-episode', epi_loss, global_step=i_episode)
             writer.add_scalar('loss-training_steps', epi_loss, global_step=training_steps)
@@ -72,14 +75,14 @@ def trainDQN():
         writer.add_scalar('steps-episode', steps, global_step=i_episode)
         writer.add_scalar('rewards-episode', epi_reward, global_step=i_episode)
 
-        if i_episode % print_interval == 0 and i_episode > 0:
+        if i_episode % cfg.print_interval == 0 and i_episode > 0:
             print("episode: {}, avg score: {:.2f}, loss: {:.2f}, buffer size: {}, epsilon:{:.2f}%".format(
-                i_episode, score / print_interval, epi_loss, model.memory.size(), epsilon * 100))
+                i_episode, score / cfg.print_interval, epi_loss, model.memory.size(), epsilon * 100))
             score = 0.0
 
-        if i_episode % test_freq == 0 and i_episode > 0:
+        if i_episode % cfg.test_freq == 0 and i_episode > 0:
             test_score = 0.0
-            for t in range(test_episodes):
+            for t in range(cfg.test_episodes):
                 epsilon = 0.0
                 obs = env.reset()
                 done = False
@@ -90,6 +93,6 @@ def trainDQN():
                     test_score += reward
                     obs = next_obs
             print('******************Test result******************')
-            print("avg score: {:.2f}".format(test_score / test_episodes))
+            print("avg score: {:.2f}".format(test_score / cfg.test_episodes))
             print('***********************************************')
     env.close()
