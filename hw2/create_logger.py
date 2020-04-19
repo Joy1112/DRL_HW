b39e82ea6c86408245c8da09b2611f0a4a48743e
joy <ss13867776368@163.com>
2020-04-19 22:21:05 +0800

    update create_logger

--- hw2/create_logger.py
+++ hw2/create_logger.py
@@ -3,22 +3,21 @@ import logging
 import time
 
 
-def create_logger(root_output_path, config):
+def create_logger(root_output_path, config, model):
     # set up logger
     if not os.path.exists(root_output_path):
         os.makedirs(root_output_path)
     assert os.path.exists(root_output_path), '{} does not exist'.format(root_output_path)
 
-    cfg_name = 'lr_' + str(config.learning_rate) + \
-               '_action_' + str(mdp_action) + '_limitH_' + str(limit_H) + '_rhoStar_' + str(rho_star)
-    final_output_path = os.path.join(root_output_path, cfg_name)
+    model_name = 'model_' + model
+    final_output_path = os.path.join(root_output_path, model_name)
     if not os.path.exists(final_output_path):
         os.makedirs(final_output_path)
     final_output_path = os.path.join(final_output_path, time.strftime('%Y-%m-%d-%H-%M'))
     if not os.path.exists(final_output_path):
         os.makedirs(final_output_path)
 
-    log_file = '{}_{}.log'.format(cfg_name, time.strftime('%Y-%m-%d-%H-%M'))
+    log_file = '{}_{}.log'.format(time.strftime('%Y-%m-%d-%H-%M'))
     head = '%(asctime)-15s %(message)s'
     logging.basicConfig(filename=os.path.join(final_output_path, log_file), format=head)
     logger = logging.getLogger()
